1e4b72a80daa5b80da99926138b473ef96b30800
Shubham Kumar <shuku@microsoft.com>
2022-12-15 18:00:15 +0530

    Added QR code for landing page

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ const morgan = require('morgan');
 const _ = require('lodash');
 const path = require('path');
 const fs = require('fs');
+const { networkInterfaces } = require('os');
+const QRCode = require('qrcode');
+
+const getQRCode = (url,i) =>{    
+    const QRCodeFile = path.join(__dirname,"img",`${i}.jpg`);
+    QRCode.toFile(QRCodeFile,url,(err)=>{
+        console.error(err);
+    });
+    console.log("QR URL:",url);
+}
+
+const nets = networkInterfaces();
+const results = []; // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+        if (net.family === familyV4Value && !net.internal) {
+            results.push(net.address);
+        }
+    }
+    console.log("IPs:",results);
+}
+
+// Generating QR Code
+let i=0;
+results.forEach((r)=>{
+    const url = `http://${r}:8383`;
+    getQRCode(url,i++);
+});
 
 const getUploadedFiles = async () =>{
     //joining path of directory 
@@ -60,9 +92,40 @@ app.listen(port, () =>
   console.log(`App is listening on port ${port}.`)
 );
 
+// Landing Page
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '/html/index.html'));
-    // res.send('OK')
+});
+
+// serving CSS
+app.get('/main.css', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/css/main.css'));
+});
+
+// serving JS
+app.get('/main.js', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/js/main.js'));
+});
+
+// serving QR img
+app.get('/QR.jpg', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/img/QR.jpg'));
+});
+
+// get all ip address
+app.get('/ip', (req, res) =>{
+    res.send(results);
+});
+
+// get QR code
+app.get('/qrcode', (req, res) =>{
+    // Access the provided 'page' and 'limt' query parameters
+    let index = req.query.i;
+    console.log('/qrcode',index);
+    // Generating QR Code
+    // const url = `http://${results[index]}:8383`;
+    // getQRCode(url,index);
+    res.sendFile(path.join(__dirname, `/img/${index}.jpg`));
 });
 
 // sendFile will go here
